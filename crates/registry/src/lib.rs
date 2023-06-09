462951e85f1e962bbb488ffc4047892a54d6ed73
curtis <zifeng.1024@gmail.com>
2023-06-09 11:22:53 +0800

    Design registry.

--- crates/registry/src/lib.rs
+++ crates/registry/src/lib.rs
@@ -1,2 +1,25 @@
+use std::collections::HashMap;
+use std::path::PathBuf;
 pub use warg_cli::commands;
 pub use warg_cli::signing;
+
+// name: grpc-service, the package name of the warg, is equal App name.
+// component_id: generate component id when publish
+// path: sha256 of the warg generated, equal the path of content.
+pub struct App {
+    name: String,
+    component_id: String,
+    // the path of component file
+    path: PathBuf,
+}
+
+pub struct AppStore {
+    base_dir: String,
+    apps: HashMap<String, App>,
+}
+
+impl AppStore {
+    pub fn fetch_component(component_id: &str) -> App {
+        todo!()
+    }
+}
